98f9ed2a7895bcefe7add8fc088748be3c6213ef
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-12-01 18:22:39 +0800

    set the data based on previous template

--- scriptPneumonia.js
+++ scriptPneumonia.js
@@ -1,6 +1,6 @@
 /* script.js */
 // TODO: change from getElementById to `querySelector('#<id>)
-function previewImage(event) {
+async function previewImage(event) {
     const imagePreview = document.getElementById('imagePreview');
     const file = event.target.files[0];
 
@@ -43,11 +43,44 @@ async function submitImage() {
         console.log('resp ok!')
         const result = await response.json()
         loadingIndicator.classList.add('hidden');
+        
+        const _data = result.data
 
-        localStorage.setItem('analysisResult', JSON.stringify(result.data));
+        localStorage.setItem('analysisResult', JSON.stringify(_data));
 
         // Redirect ke halaman kedua
-        window.location.href = 'result.html';
+        // window.location.href = 'result.html';
+        let resultElement = document.querySelector('#result');
+        resultElement.textContent = _data.result
+
+        let resultContainer = document.querySelector('#result-container');
+        resultContainer.classList.remove('hidden');
+
+        let resultDetailContainerElement = document.querySelector('#resultDetail');
+        let {normal, bacteria, virus} = _data._prediction_value
+        
+        function createResultElement(resultType, value){
+            // WARNING: probably not safe (may injected using client side scripting)
+            let resultElementHTML = `
+                <div class="flex flex-row gap-x-2 justify-center">
+                    <div>
+                        <p class="capitalize">${resultType}: </p>
+                    </div>
+
+                    <div>
+                        <p>${value}</p>
+                    </div>
+                </div>
+            `
+            
+            let resultElement = document.createElement('div')
+            resultElement.innerHTML = resultElementHTML
+            resultDetailContainerElement.appendChild(resultElement)
+        }
+
+        createResultElement('normal', normal)
+        createResultElement('bacteria', bacteria)
+        createResultElement('virus', virus)
     }
     else {
         console.log('resp error')
